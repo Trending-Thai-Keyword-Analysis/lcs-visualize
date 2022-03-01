5edb1d7be2099e827be4265f5982c52d2d4f4fd4
Puwadol L <alpe_panda@hotmail.co.th>
2022-03-01 23:28:13 +0700

    update new design and method

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 from datetime import datetime,date, timedelta
 from re import M
+import plotly.express as px
 import streamlit as st
 import pandas as pd
 import numpy as np
 from stqdm import stqdm
 import os
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, JsCode
 
 @st.cache(suppress_st_warning=True)
-def SMA(trend,ndate,slot,before):
-    mv = []
-    date_picker = []
+def SMA(trend,slot,before):
+    mv = [0] * (slot - 1)
+    sdate = date(2021,10,slot)
     for i in range(before):
-        start = ndate - timedelta(i)
+        start = sdate + timedelta(i)
         # print(start)
-        date_picker.append(str(start))
         data = pd.DataFrame([])
         for j in range(slot):
             temp_date = start - timedelta(j)
@@ -34,16 +35,14 @@ def SMA(trend,ndate,slot,before):
         
         avg = data['match'].sum()/slot
         mv.append(avg)
-    return date_picker,mv
+    return mv
 
 @st.cache(suppress_st_warning=True)
-def EMA(trend,sdate,slot,before):
-    mv = []
-    date_picker = []
+def EMA(trend,slot,before):
+    mv = [0] * (slot - 1)
+    sdate = date(2021,10,slot)
     for i in range(before):
         start = sdate + timedelta(i)
-        # print(str(start))
-        date_picker.append(str(start))
         # First Round Of EMA = Calculate SMA
         if i == 0:
             data = pd.DataFrame([])
@@ -83,7 +82,29 @@ def EMA(trend,sdate,slot,before):
                 avg = (0 - last_avg) * (2 / (1 + slot)) + last_avg
                 mv.append(avg)
     
-    return date_picker,mv
+    return mv
+
+@st.cache(suppress_st_warning=True)
+def get_freq(trend,ndate):
+    before = ndate - date(2021,9,30)
+    before = int(before.total_seconds()/(60*60*24))
+    freq_list = []
+    date_picker = []
+    for i in range(before):
+        start = date(2021, 10, 1) + timedelta(i)
+        date_picker.append(str(start))
+        temp_date = '_'.join(str(start).split('-'))
+        temp_trend = pd.read_json('./assets/lcs_result/'+str(temp_date)+'.json',encoding="utf8")
+        temp_trend = temp_trend[temp_trend['LCS'] == trend]
+        temp_trend.drop('total_match',axis='columns',inplace = True)
+
+        if len(temp_trend) != 0:
+            freq_list.append(temp_trend.iloc[0]["match"])
+        else:
+            freq_list.append(0)
+    
+    return freq_list, date_picker
+
 
 
 @st.cache(suppress_st_warning=True)
@@ -110,25 +131,67 @@ with c1:
     # print(news)
     df = pd.read_json('./assets/lcs_result/'+news+'.json',encoding="utf8")
     st.subheader('Trending Result')
-    st.dataframe(df[['LCS','match']])
+
+    cellsytle_jscode = JsCode("""
+    function(params) {
+        return {
+            'fontSize': '125%'
+        }
+    };
+    """)
+
+    #Infer basic colDefs from dataframe types
+    gb = GridOptionsBuilder.from_dataframe(df[['LCS','match']])
+    gb.configure_default_column(resizable=False, cellStyle=cellsytle_jscode)
+    gb.configure_column("LCS", type="string", minWidth=300)
+    gb.configure_selection(selection_mode='single', pre_selected_rows=[0])
+    gb.configure_pagination(paginationAutoPageSize=True)
+    # gb.configure_grid_options(domLayout='normal')
+    gridOptions = gb.build()
+
+
+    grid_response = AgGrid(
+        df , 
+        gridOptions=gridOptions, 
+        theme="streamlit", 
+        update_mode=GridUpdateMode.SELECTION_CHANGED, 
+        data_return_mode=DataReturnMode.AS_INPUT, 
+        fit_columns_on_grid_load=True, 
+        allow_unsafe_jscode=True
+        )
+    df = grid_response["data"]
+    selected = grid_response['selected_rows']
+    selected_df = pd.DataFrame(selected)
+    trend = ""
+    if not selected_df.empty:
+        trend = selected_df.iloc[0]["LCS"]
+    # st.write(df[['LCS','match']])
 ############################################
 
 with c2:
     st.subheader('Moving Average')
-    with st.form('Moving Average'):
-        st.session_state.trend = st.text_input('Word','ทิดไพรวัลย์')
-        submitted2 = st.form_submit_button('Submit')
+    option = st.selectbox('Moving Average',
+     ('Simple Moving Averge', 'Exponential Moving Average'))
+    st.markdown(f'<p style="font-size:25px;text-align: center;margin-bottom: -1rem"> {trend} </p>', unsafe_allow_html=True)
     try:
-        before =date(2021,12,31) - date(2021,10,4)
+        before = date(2021,12,31) - date(2021,10,4)
         before = int(before.total_seconds()/(60*60*24))
-        # print(before)
-        date_all,mv = EMA(st.session_state.trend,date(2021,10,5),5,before)
-        # print(date_all)
-        result = pd.DataFrame({
-            'date':date_all,
-            'simple_moving_average':mv
-        })
-        st.subheader('Moving Average Graph')
-        st.line_chart(result.rename(columns={'date':'index'}).set_index('index'))
+        freq_trend, date_all = get_freq(trend, date(2021,12,31))
+
+        mv, legend = [], ""
+
+        if option == "Simple Moving Averge":
+            mv = SMA(trend,5,before)
+            legend = "SMA"
+        elif option == "Exponential Moving Average":
+            mv = EMA(trend,5,before)
+            legend = "EMA"
+
+        fig = px.line(x=date_all, y=mv, color=px.Constant(legend), labels=dict(x="date", y="frequency", color="Legend"), markers=True)
+        fig.add_bar(x=date_all, y=freq_trend, name="frequency")
+
+        st.plotly_chart(fig, use_container_width=True)
+        # st.write(gridOptions)
+
     except FileNotFoundError:
         st.error("ขอบเขตของช่วงเวลาที่ใช้ในการคำนวณเกิน กรุณาปรับ Window Slot หรือ Day Before")
